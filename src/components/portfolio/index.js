e9847014a2e8a6932a3a94592b4cdf934e0adca8
Anthony Tulumello <anthonytulumello@gmail.com>
2020-09-01 16:55:45 -0400

    Update Information
    Second Slide

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -90,10 +90,6 @@ class Portfolio extends React.Component {
                                         <InlineIcon icon={githubIcon} style={{margin: '0 4px 3px 0'}} />
                                         View on GitHub
                                     </Button>
-                                    <Button onClick={() => {window.open(this.project.fseCalculator.live, '_blank')}} className="portfolio-button d-inline" outline>
-                                        <InlineIcon icon={githubIcon} style={{margin: '0 4px 3px 0'}} />
-                                        View Live
-                                    </Button>
                                 </Col>
                             </Col>
                             <Col xs={12} lg={6} className="text-center text-lg-left">
@@ -107,7 +103,17 @@ class Portfolio extends React.Component {
                                         <a href="https://www.gatsby.com" target="_blank" rel="noreferrer" aria-label="Gatsby" className="portfolio-icon d-inline" ><Icon icon={sassIcon} /></a>
                                     </Col>
                                     <Col xs={12}>
-                                        <p className="mt-2">A simple loan calculator built for an aviation economy game. NodeJs communicates with Google Firebase in order to dynamically update available funds from the game world api.</p>
+                                        <p className="mt-2">My personal website you are viewing now, wrapped with fullpagejs. My goal was to show some of my knowledge while being as simple as possible.</p>
+                                    </Col>
+                                    <Col xs={12}>
+                                        <p className="mb-2">NPM Packages Used:</p>
+                                    </Col>
+                                    <Col xs={12}>
+                                        <Badge>fullpagejs</Badge>
+                                        <Badge>reactstrap</Badge>
+                                        <Badge>react-helmet</Badge>
+                                        <Badge>react-robots-txt</Badge>
+                                        <Badge>iconify/react</Badge>
                                     </Col>
                                 </Row>
                             </Col>
